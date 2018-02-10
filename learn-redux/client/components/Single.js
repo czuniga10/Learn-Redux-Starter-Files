cc0a11d600cd706d78443352385aa88c189dae9b
Chad <chad.zuniga10@gmail.com>
2018-02-09 22:37:30 -0700

    finished app

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        //index of post
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        //get post
+        const post = this.props.posts[i];
+        const postComments = this.props.comments[this.props.params.postId] || [];
+
         return(
             <div className="single-photo">
-            I'm the single photo
+                <Photo i={i} post={post} {... this.props}/>
+                <Comments postComments= {postComments} {... this.props}/>
             </div>
         );
     }
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -3,8 +3,37 @@
 // 1. action (info about what's happening)
 // 2. copy of current state
 
+function postComments( state=[], action) {
+    switch (action.type){
+        case 'ADD_COMMENT':
+            //return existing state plus new comment
+            return [...state,{
+                user: action.author,
+                text: action.comment
+            }];
+        case 'REMOVE_COMMENT':
+            return [
+                // start from the beginning to where you're deleting
+                ...state.slice(0,action.i),
+                // after the deleted one till the end
+                ...state.slice(action.i + 1)
+            ]
+            return state;
+        default:
+            return state;
+    }
+    return state;
+}
+
 function comments( state=[], action) {
-    console.log(state, action);
+    if(typeof action.postId !== 'undefined') {
+        return {
+            // take the current state
+            ...state,
+            // overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
